e1a640dcee3e3ef119f890a2505c83c4b959827a
Craig Clayton <craig@thedev.me>
2016-07-26 22:22:46 -0700

    create starter project

---
+++ SoccerDemo/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  SoccerDemo
+//
+//  Created by Craig Clayton on 7/11/16.
+//  Copyright (c) 2016 Cocoa Academy. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+}
---
+++ SoccerDemo/GameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameViewController.swift
+//  SoccerDemo
+//
+//  Created by Craig Clayton on 7/11/16.
+//  Copyright (c) 2016 Cocoa Academy. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameController
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
